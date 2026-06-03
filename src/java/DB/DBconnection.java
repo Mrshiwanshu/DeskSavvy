28be8a6dd7b25d63e816eed246e0e3a423535f17
shiwanshuchourasia <shiwanshu7545@gmail.com>
2026-06-03 16:07:45 +0530

    Upgrade MySQL connector to 8.0.28 and switch to com.mysql.cj.jdbc.Driver

--- src/java/DB/DBconnection.java
+++ src/java/DB/DBconnection.java
@@ -11,7 +11,7 @@ public class DBconnection {
   {
   try
   {
-  Class.forName("com.mysql.jdbc.Driver");
+  Class.forName("com.mysql.cj.jdbc.Driver");
   String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://localhost:3306/virtual_office";
   String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
   String dbPassword = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "root";
