2c08fae0da9d181981587636c9507effa090c1e8
Mrshiwanshu <shiwanshu7545@gmail.com>
2026-06-27 08:56:02 +0530

    Update changepassword1.java

--- src/java/changepassword1.java
+++ src/java/changepassword1.java
@@ -1,110 +1,146 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import DB.DBconnection;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author AJAY YADAV
- */
+@WebServlet("/changepassword1")
 public class changepassword1 extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-              HttpSession st=request.getSession(true);
-            
-         String old=request.getParameter("t1");
-            String npass=request.getParameter("t2");
-            String user=(String)st.getAttribute("user2");
-            DBconnection db=new DBconnection();
-           
-            db.pstmt=db.con.prepareStatement("select pass from company_login where id='"+user+"'");
-            db.rst=db.pstmt.executeQuery();
-            if(db.rst.next())
-            {
-             String pass=db.rst.getString(1);
-             if(pass.equals(old))
-             {
-             
-             db.pstmt=db.con.prepareStatement("update company_login set pass=? where id='"+user+"'");
-             db.pstmt.setString(1, npass);
-             int i=db.pstmt.executeUpdate();
-             if(i>0)
-             {
-            response.sendRedirect("empchangePass.jsp?msg=Password change successfull..");
-              
-             }
-             }
-             
-             else
-             {
-             response.sendRedirect("empchangePass.jsp?msg=Old Password not match!!!!");
-             }
+
+        try {
+
+            // Get existing session only
+            HttpSession st = request.getSession(false);
+
+            if (st == null) {
+                response.sendRedirect(
+                    "support.jsp?msg=Please Login First"
+                );
+                return;
             }
-            
-        
-        } catch(Exception e) {            
+
+            // Get form values
+            String oldPass = request.getParameter("t1");
+            String newPass = request.getParameter("t2");
+
+            // Get logged-in employee id from session
+            String user = (String) st.getAttribute("user2");
+
+            if (user == null) {
+                response.sendRedirect(
+                    "support.jsp?msg=Session Expired"
+                );
+                return;
+            }
+
+            // Database connection
+            DBconnection db = new DBconnection();
+
+            if (db.con == null) {
+                response.sendRedirect(
+                    "empchangePass.jsp?msg=Database Connection Failed"
+                );
+                return;
+            }
+
+            // Check current password
+            db.pstmt = db.con.prepareStatement(
+                "SELECT password FROM emp_login WHERE emp_id=?"
+            );
+
+            db.pstmt.setString(1, user);
+
+            db.rst = db.pstmt.executeQuery();
+
+            if (db.rst.next()) {
+
+                String dbPassword =
+                        db.rst.getString("password");
+
+                if (dbPassword.equals(oldPass)) {
+
+                    // Update password
+                    db.pstmt = db.con.prepareStatement(
+                        "UPDATE emp_login SET password=? WHERE emp_id=?"
+                    );
+
+                    db.pstmt.setString(1, newPass);
+                    db.pstmt.setString(2, user);
+
+                    int i = db.pstmt.executeUpdate();
+
+                    if (i > 0) {
+
+                        response.sendRedirect(
+                            "empchangePass.jsp?msg=Password Changed Successfully"
+                        );
+
+                    } else {
+
+                        response.sendRedirect(
+                            "empchangePass.jsp?msg=Password Not Updated"
+                        );
+                    }
+
+                } else {
+
+                    response.sendRedirect(
+                        "empchangePass.jsp?msg=Old Password Not Match"
+                    );
+                }
+
+            } else {
+
+                response.sendRedirect(
+                    "empchangePass.jsp?msg=User Not Found"
+                );
+            }
+
+            // Close resources
+            if (db.rst != null)
+                db.rst.close();
+
+            if (db.pstmt != null)
+                db.pstmt.close();
+
+            if (db.con != null)
+                db.con.close();
+
+        } catch (Exception e) {
+
             e.printStackTrace();
+
+            response.sendRedirect(
+                "empchangePass.jsp?msg=Error Occurred"
+            );
         }
     }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
             throws ServletException, IOException {
+
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
             throws ServletException, IOException {
+
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        return "Change Password Servlet";
+    }
 }
